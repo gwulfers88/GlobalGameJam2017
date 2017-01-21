1c4ba0474ca7c60f3639df02a1aeec4b8406f5f7
Ashley Hearne <Ashley Hearne>
2017-01-21 07:48:36 -0800

    Update on menus, working main menus, working credit menu, player pause started, no yet

--- GGJam2017/Intermediate/Build/Win64/UE4Editor/Development/GGJam2017/GGJam2017.h
+++ GGJam2017/Intermediate/Build/Win64/UE4Editor/Development/GGJam2017/GGJam2017.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_GGJAM2017_H__
 #define __AUTO_GGJAM2017_H__
-//Last Write: 1/21/2017 12:13:01 PM +00:00
-#include "C:\Users\natha\Desktop\GGJam2017\Source\GGJam2017\GGJam2017.h"
+//Last Write: 1/21/2017 12:57:34 PM +00:00
+#include "C:\Users\persi\Documents\GameJam\GlobalGameJam2017\GGJam2017\Source\GGJam2017\GGJam2017.h"
 #endif//__AUTO_GGJAM2017_H__
